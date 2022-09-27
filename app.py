aedb628eb563062ec89f69dbb45540324f062312
lumburovskalina <104008550+lumburovskalina@users.noreply.github.com>
2022-09-27 15:21:31 +0200

    Release v1.3.0 (#11)
    Release v1.3.0

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ CONFIG_URI = os.getenv("CONFIG")
 
 class ErrorCodes:
     UNRECOGNIZED_USER = "UNRECOGNIZED_USER"
+    FORBIDDEN_USER = "FORBIDDEN_USER"
     PROJECT_NOT_FOUND = "PROJECT_NOT_FOUND"
 
 
@@ -22,6 +23,23 @@ def get_user_id_from_request(request):
     whoami_dict = resp.json()
     user_id = whoami_dict["identity"]["id"]
     return user_id
+
+
+def handle_response(response):
+    if response.status_code == status.HTTP_200_OK:
+        return responses.JSONResponse(
+            status_code=status.HTTP_200_OK, content=response.json()
+        )
+    elif response.status_code == status.HTTP_404_NOT_FOUND:
+        return responses.JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content={"error_code": ErrorCodes.PROJECT_NOT_FOUND},
+        )
+    elif response.status_code == status.HTTP_403_FORBIDDEN:
+        return responses.JSONResponse(
+            status_code=status.HTTP_403_FORBIDDEN,
+            content={"error_code": ErrorCodes.FORBIDDEN_USER},
+        )
 
 
 @app.get("/project/{project_id}/export")
@@ -35,7 +53,7 @@ def get_export(request: Request, project_id: str, num_samples: Optional[int] = N
         )
     url = f"{BASE_URI}/project/{project_id}/export"
     resp = requests.get(url, params={"user_id": user_id, "num_samples": num_samples})
-    return responses.JSONResponse(status_code=status.HTTP_200_OK, content=resp.json())
+    return handle_response(resp)
 
 
 @app.get("/project/{project_id}/lookup_list/{lookup_list_id}")
@@ -49,11 +67,11 @@ def get_lookup_list(request: Request, project_id: str, lookup_list_id: str):
         )
     url = f"{BASE_URI}/project/{project_id}/knowledge_base/{lookup_list_id}"
     resp = requests.get(url, params={"user_id": user_id})
-    return responses.JSONResponse(status_code=status.HTTP_200_OK, content=resp.json())
+    return handle_response(resp)
 
 
-@app.post("/project/{project_id}/import")
-async def post_import(request: Request, project_id: str):
+@app.post("/project/{project_id}/import_file")
+async def post_import_file(request: Request, project_id: str):
     try:
         user_id = get_user_id_from_request(request)
     except KeyError:
@@ -62,7 +80,7 @@ async def post_import(request: Request, project_id: str):
             content={"error_code": ErrorCodes.UNRECOGNIZED_USER},
         )
     request_body = await request.json()
-    url = f"{BASE_URI}/project/{project_id}/import"
+    url = f"{BASE_URI}/project/{project_id}/import_file"
     resp = requests.post(
         url,
         json={
@@ -72,7 +90,55 @@ async def post_import(request: Request, project_id: str):
             "file_import_options": request_body.get("file_import_options"),
         },
     )
+    return handle_response(resp)
+
+
+@app.post("/project/{project_id}/import_json")
+async def post_import_json(request: Request, project_id: str):
+    try:
+        user_id = get_user_id_from_request(request)
+    except KeyError:
+        return responses.JSONResponse(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            content={"error_code": ErrorCodes.UNRECOGNIZED_USER},
+        )
+    request_body = await request.json()
+    url = f"{BASE_URI}/project/{project_id}/import_json"
+    resp = requests.post(
+        url,
+        json={
+            "user_id": user_id,
+            "records": request_body["records"],
+            "request_uuid": request_body["request_uuid"],
+            "is_last": request_body["is_last"],
+        },
+    )
     return responses.JSONResponse(status_code=status.HTTP_200_OK, content=resp.json())
+
+
+@app.post("/project/{project_id}/associations")
+async def post_associations(request: Request, project_id: str):
+    try:
+        user_id = get_user_id_from_request(request)
+    except KeyError:
+        return responses.JSONResponse(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            content={"error_code": ErrorCodes.UNRECOGNIZED_USER},
+        )
+    request_body = await request.json()
+    url = f"{BASE_URI}/project/{project_id}/associations"
+    resp = requests.post(
+        url,
+        json={
+            "user_id": user_id,
+            "associations": request_body["associations"],
+            "indices": request_body["indices"],
+            "name": request_body["name"],
+            "label_task_name": request_body["label_task_name"],
+            "source_type": request_body["source_type"],
+        },
+    )
+    return handle_response(resp)
 
 
 @app.get("/project/{project_id}")
@@ -86,15 +152,7 @@ def get_details(request: Request, project_id: str):
         )
     url = f"{BASE_URI}/project/{project_id}"
     resp = requests.get(url, params={"user_id": user_id})
-    if resp.status_code == 200:
-        return responses.JSONResponse(
-            status_code=status.HTTP_200_OK, content=resp.json()
-        )
-    else:
-        return responses.JSONResponse(
-            status_code=status.HTTP_404_NOT_FOUND,
-            content={"error_code": ErrorCodes.PROJECT_NOT_FOUND},
-        )
+    return handle_response(resp)
 
 
 @app.get("/project/{project_id}/import/base_config")
@@ -108,7 +166,7 @@ def get_base_config(request: Request, project_id: str):
         )
     url = f"{CONFIG_URI}/base_config"
     resp = requests.get(url)
-    return responses.JSONResponse(status_code=status.HTTP_200_OK, content=resp.json())
+    return handle_response(resp)
 
 
 @app.get("/project/{project_id}/import/task/{task_id}")
@@ -122,4 +180,4 @@ def get_details(request: Request, project_id: str, task_id: str):
         )
     url = f"{BASE_URI}/project/{project_id}/import/task/{task_id}"
     resp = requests.get(url, params={"user_id": user_id})
-    return responses.JSONResponse(status_code=status.HTTP_200_OK, content=resp.json())
+    return handle_response(resp)
